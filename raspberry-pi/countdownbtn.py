f6283ed5c0dac22ea577ae5c6083c2d8351bfb73
zsiller38 <zsiller38@charlottesvilleschools.org>
2023-09-12 14:05:42 -0400

    countdownbtn code

---
+++ raspberry-pi/countdownbtn.py
@@ -0,0 +1,24 @@
+#type: ignore
+import time
+import board
+import digitalio
+ledg=digitalio.DigitalInOut(board.GP15)
+ledg.direction=digitalio.Direction.OUTPUT
+ledr=digitalio.DigitalInOut(board.GP16)
+ledr.direction=digitalio.Direction.OUTPUT
+btn = DigitalInOut(board.GP0)
+btn.pull = digitalio.Pull.UP
+btn.direction = Direction.INPUT
+
+
+if btn.value == True
+    for x in range (10,0,-1):
+        print(x)
+        ledr.value=True
+        time.sleep(.5)
+        ledr.value=False
+        time.sleep(.5)
+        if x <= 1:
+            print("Liftoff")
+            ledg.value=True
+            time.sleep(2)
