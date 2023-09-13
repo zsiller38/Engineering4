bd9c9a8532005b8853d1a45f2ab4a0c7ab453a55
zsiller38 <zsiller38@charlottesvilleschools.org>
2023-09-13 13:46:12 -0400

    Updated comments

--- raspberry-pi/countdown.py
+++ raspberry-pi/countdown.py
@@ -1,8 +1,10 @@
 #type: ignore
+#Zachary Siller E4 9/6/2023
+#Rocket countdown from 10 using the serial monitor
 import time
 
-for x in range (10,0,-1):
-    print(x)
+for x in range (10,0,-1): #For loop will start counting down from 10 and stop at 1
+    print(x) #Prints the number the for loop is on
     time.sleep(1)
-    if x == 1:
+    if x == 1: #Once the loop counts to 1 it will print liftoff
         print("Liftoff")
--- raspberry-pi/countdownwithLED.py
+++ raspberry-pi/countdownwithLED.py
@@ -1,20 +1,22 @@
 #type: ignore
+#Zachary Siller E4 9/12/2023
+# Same as rocket countdown but now using a red light to signal each count and a green light when liftoff occurs
 import time
 import board
 import digitalio
-ledg=digitalio.DigitalInOut(board.GP15)
+ledg=digitalio.DigitalInOut(board.GP15) #Lines 7-10 led set up
 ledg.direction=digitalio.Direction.OUTPUT
 ledr=digitalio.DigitalInOut(board.GP16)
 ledr.direction=digitalio.Direction.OUTPUT
 
-for x in range (10,0,-1):
-    print(x)
-    ledr.value=True
+for x in range (10,0,-1): #For loop will start counting down from 10 and stop at 1
+    print(x)   #Prints the number the for loop is on
+    ledr.value=True #Turns light on and off
     time.sleep(.5)
     ledr.value=False
     time.sleep(.5)
     if x <= 1:
-        print("Liftoff")
+        print("Liftoff") 
         ledg.value=True
         time.sleep(2)
         
