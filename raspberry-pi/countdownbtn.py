cbf189cd0e5ae0306637cb3ed21514ae733500bc
zsiller38 <zsiller38@charlottesvilleschools.org>
2023-09-14 13:57:12 -0400

    finalbtn

--- raspberry-pi/countdownbtn.py
+++ raspberry-pi/countdownbtn.py
@@ -6,24 +6,26 @@ ledg=digitalio.DigitalInOut(board.GP15)
 ledg.direction=digitalio.Direction.OUTPUT
 ledr=digitalio.DigitalInOut(board.GP16)
 ledr.direction=digitalio.Direction.OUTPUT
-btn = DigitalInOut(board.GP19)
-btn.pull = digitalio.Pull.UP
-btn.direction = Direction.INPUT
-
+btn = digitalio.DigitalInOut(board.GP19)
+btn.pull = digitalio.Pull.DOWN
+btn.direction = digitalio.Direction.INPUT
 print("Ready for launch")
 
-while btn.value == True
-   
-    for x in range (10,0,-1):
-        print(x)
-        ledr.value=True
-        time.sleep(.5)
-        ledr.value=False
-        time.sleep(.5)
+
+while btn.value == False:
+    pass
+    
+for x in range (10,0,-1):
+    print(x)
+    ledr.value=True
+    time.sleep(.5)
+    ledr.value=False
+    time.sleep(.5)
         
     if x <= 1:
-            print("Liftoff")
-            ledg.value=True
-            time.sleep(2)
-            ledg.value=False
-            break
+        print("Liftoff")
+        ledg.value=True
+        time.sleep(2)
+        ledg.value=False
+    
+        
