358502568251b383512dd41c9ebea756774ca830
zsiller38 <zsiller38@charlottesvilleschools.org>
2023-09-14 13:23:38 -0400

    hio

--- raspberry-pi/countdownbtn.py
+++ raspberry-pi/countdownbtn.py
@@ -6,12 +6,13 @@ ledg=digitalio.DigitalInOut(board.GP15)
 ledg.direction=digitalio.Direction.OUTPUT
 ledr=digitalio.DigitalInOut(board.GP16)
 ledr.direction=digitalio.Direction.OUTPUT
-btn = DigitalInOut(board.GP0)
+btn = DigitalInOut(board.GP19)
 btn.pull = digitalio.Pull.UP
 btn.direction = Direction.INPUT
-
+countdown=False
 
 if btn.value == True
+   
     for x in range (10,0,-1):
         print(x)
         ledr.value=True
