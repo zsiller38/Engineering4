67f9116086979f545de5f905bce76f493cb42693
zsiller38 <zsiller38@charlottesvilleschools.org>
2023-09-18 14:06:57 -0400

    code comments

--- countdownservo.py
+++ countdownservo.py
@@ -1,4 +1,6 @@
 #type: ignore
+#Zachary Siller E4 9/15/2023
+# Turn a servo 180 degrees to simulate rocket takeoff
 import time
 import board
 import digitalio
@@ -12,7 +14,7 @@ ledr.direction=digitalio.Direction.OUTPUT
 btn = digitalio.DigitalInOut(board.GP19)
 btn.pull = digitalio.Pull.DOWN
 btn.direction = digitalio.Direction.INPUT
-pwm_servo = pwmio.PWMOut(board.GP3, duty_cycle=2 ** 15, frequency=50)
+pwm_servo = pwmio.PWMOut(board.GP3, duty_cycle=2 ** 15, frequency=50) #Setup servo
 servo1 = servo.Servo(pwm_servo, min_pulse=500, max_pulse=2500)
 
 servo1.angle = 0
@@ -25,7 +27,7 @@ print("Ready for launch")
 while btn.value == False:
     pass
     
-for x in range (10,0,-1):
+for x in range (10,0,-1): # "-1" makes counter count down, excludes 0, so stops at 1
     print(x)
     ledr.value=True
     time.sleep(.5)
@@ -34,7 +36,7 @@ for x in range (10,0,-1):
         
     if x <= 1:
         print("Liftoff")
-        servo1.angle=180
+        servo1.angle=180 #rotates servo
         ledg.value=True
         time.sleep(2)
         ledg.value=False
--- raspberry-pi/countdownbtn.py
+++ raspberry-pi/countdownbtn.py
@@ -1,4 +1,7 @@
 #type: ignore
+#Zachary Siller E4 9/14/2023
+# Trigger the countdown using a button 
+
 import time
 import board
 import digitalio
@@ -12,11 +15,11 @@ btn.direction = digitalio.Direction.INPUT
 print("Ready for launch")
 
 
-while btn.value == False:
-    pass
+while btn.value == False: #Always checks if button has been pressed or not
+    pass #Allows loop to function without something in it
     
-for x in range (10,0,-1):
-    print(x)
+for x in range (10,0,-1): # 21-32 same as previous LED countdown
+    print(x) # "-1" makes counter count down, excludes 0, so stops at 1
     ledr.value=True
     time.sleep(.5)
     ledr.value=False
