a7ca6608eac65f007974fc53d74e344dc0ee65ef
zsiller38 <zsiller38@charlottesvilleschools.org>
2023-09-22 14:07:58 -0400

    code comments

--- raspberry-pi/gyrocode.py
+++ raspberry-pi/gyrocode.py
@@ -1,4 +1,6 @@
 #type: ignore
+#Zachary Siller E4 9/20/2023
+#Use a mpu6060 to track angular accelertaion of the x, y, and z axis. Use an f string to print the data in a readable manner.
 import time
 import board
 import digitalio
@@ -6,7 +8,7 @@ import adafruit_mpu6050
 import busio
 
 
-sda_pin = board.GP15
+sda_pin = board.GP15 #11-14 pin setup and initialization.
 scl_pin = board.GP14
 i2c = busio.I2C(scl_pin, sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c)
@@ -14,6 +16,6 @@ mpu = adafruit_mpu6050.MPU6050(i2c)
 
 
 while True
-    Acceleration = mpu.acceleration
+    Acceleration = mpu.acceleration #Reads the acceleration and decomposes it into x, y, and z
     Xa,Ya,Za=Acceleration
-    print(f" Acceleration X{Xa:.3f}, Y{Ya:.3f}, Z{Za:.3f}.")    
+    print(f" Acceleration X{Xa:.3f}, Y{Ya:.3f}, Z{Za:.3f}.")    # prints the acceleration values and rounds the # using :.3f
--- raspberry-pi/gyroled.py
+++ raspberry-pi/gyroled.py
@@ -1,18 +1,20 @@
 #type: ignore
+#Zachary Siller E4 9/22/2023
+#Record and print acceleration from mpu6050. Blink a light whenever the board is rotated more than 90 degrees.
 import time
 import board
 import digitalio
 import adafruit_mpu6050 
 import busio
 
-ledr=digitalio.DigitalInOut(board.GP0) #Lines 7-10 led set up
+ledr=digitalio.DigitalInOut(board.GP0) #Lines 10-16 led set up
 ledr.direction=digitalio.Direction.OUTPUT
 
 sda_pin = board.GP14
 scl_pin = board.GP15
 i2c = busio.I2C(scl_pin, sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c)
-tiltT = .5
+tiltT = .5 #tilt variable 
 ledr.value=False
 
 while True:
@@ -20,8 +22,8 @@ while True:
     Xa,Ya,Za=Acceleration
     print(f" Acceleration X:{Xa:.3f}, Y:{Ya:.3f}, Z:{Za:.3f}.")    
     time.sleep(.1)
-    if (tiltT) > (Za):
-        ledr.value=True 
+    if (tiltT) > (Za): #checks if the angular acceleration around the z axis is less than the tilt variable.
+        ledr.value=True #this works because when the board is turned more than 90 degrees the z acceleration is less than the tilt var.
         print("led on")
         time.sleep(.1)
     else:
