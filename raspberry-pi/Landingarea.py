f3c345dca983f0cd58f54d2faaf71eb5abb891ac
zsiller38 <zsiller38@charlottesvilleschools.org>
2024-01-18 14:10:33 -0500

    comments

--- raspberry-pi/Landingarea.py
+++ raspberry-pi/Landingarea.py
@@ -6,24 +6,24 @@
 #import digitalio
 
 
-def trianglearea (x1y1, x2y2, x3y3):
+def trianglearea (x1y1, x2y2, x3y3): #Function that calculates triangle area and uses a try loop to eliminate invalid inputs
     try:
-        x1y1=x1y1.split(',')
+        x1y1=x1y1.split(',')  #Splits user input into 6 components using commas
         x2y2=x2y2.split(',')
         x3y3=x3y3.split(',')
     
-        x1=float(x1y1 [0])
+        x1=float(x1y1 [0]) #Turns the split input into floats
         y1=float(x1y1 [1])
         x2=float(x2y2 [0])
         y2=float(x2y2 [1])
         x3=float(x3y3 [0])
         y3=float(x3y3 [1])
-        area = abs((1/2)*(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2)))
-        if area == 0:
+        area = abs((1/2)*(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2))) #Calculates are using a formula.
+        if area == 0: #If the points given do not create a triangle this statement will be printed.
             print("not a triangle") 
-        else:
+        else: #If the inputs do create a valid triangle
             return area      
-    except:
+    except: #Runs if the try loop fails indicating there was an input error.
         print("invalid imput")
         area = 0
         return area
@@ -31,11 +31,11 @@ def trianglearea (x1y1, x2y2, x3y3):
 
 while True:
     
-    coord1 = input('Coordinates for point 1:')
+    coord1 = input('Coordinates for point 1:')  #Grabs user input and stores it
     coord2 = input('Coordinates for point 2:')
     coord3 = input('Coordinates for point 3:')
 
-    area = trianglearea(coord1, coord2, coord3)
+    area = trianglearea(coord1, coord2, coord3) #Calls the function.
     if area==0:
         continue
     
--- raspberry-pi/OLEDLanding.py
+++ raspberry-pi/OLEDLanding.py
@@ -1,6 +1,6 @@
 #type: ignore
 # Zachary Siller
-# Landing Area Part 1 10/20/2023
+# Landing Area Part 2 10/20/2023
 
 import board
 import digitalio
@@ -10,13 +10,13 @@ import adafruit_displayio_ssd1306
 import terminalio
 import displayio
 import adafruit_display_shapes
-from adafruit_display_shapes.triangle import Triangle
+from adafruit_display_shapes.triangle import Triangle #import required shapes
 from adafruit_display_shapes.line import Line
 from adafruit_display_shapes.circle import Circle
 
 displayio.release_displays()
 
-sda_pin = board.GP14
+sda_pin = board.GP14    #Lines 19-30 setup for pins and OLED display
 scl_pin = board.GP15
 i2c = busio.I2C(scl_pin, sda_pin)
 
@@ -29,7 +29,7 @@ text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=2, y=5)
 splash.append(text_area) 
 display.show(splash)   
 
-xaxisx1=0
+xaxisx1=0       #Lines 32-39 variables to account for offset in OLED coordinate grid
 xaxisx2=128
 xaxisy1=32
 xaxisy2=32
@@ -38,45 +38,45 @@ yaxisx2=64
 yaxisy1=0
 yaxisy2=64
 
-circle = Circle(64, 32, 2, outline=0xFFFF00)
+circle = Circle(64, 32, 2, outline=0xFFFF00) #Lines 41-46 creates origin and x/y axis shapes
 splash.append(circle)
 xaxis = Line(xaxisx1,xaxisy1,xaxisx2,xaxisy2, color=0xFFFF00)
 splash.append(xaxis)
 yaxis = Line(yaxisx1,yaxisy1,yaxisx2,yaxisy2, color=0xFFFF00)
 splash.append(yaxis)
 
-def trianglearea (x1y1, x2y2, x3y3):
+def trianglearea (x1y1, x2y2, x3y3): #Function that calculates triangle area and uses a try loop to eliminate invalid inputs
     try:
-        x1y1=x1y1.split(',')
+        x1y1=x1y1.split(',') #Splits user input into 6 components using commas
         x2y2=x2y2.split(',')
         x3y3=x3y3.split(',')
     
-        x1=float(x1y1 [0])
+        x1=float(x1y1 [0]) #Turns the split input into floats
         y1=float(x1y1 [1])
         x2=float(x2y2 [0])
         y2=float(x2y2 [1])
         x3=float(x3y3 [0])
         y3=float(x3y3 [1])
-        area = abs((1/2)*(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2)))
-        if area == 0:
+        area = abs((1/2)*(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2))) #Calculates are using a formula.
+        if area == 0: #If the points given do not create a triangle this statement will be printed.
             print("not a triangle") 
             avtext = f"Not a triangle"
             text_area.text = avtext
         else:
-            avtext = f" Area {area} km^2"
+            avtext = f" Area {area} km^2" #If the inputs do create a valid triangle
             text_area.text = avtext
-            Tx1=x1+64
+            Tx1=x1+64       #lines 68-73 modifier to account for OLED coordinate system
             Ty1=-y1+32
             Tx2=x2+64
             Ty2=-y2+32
             Tx3=x3+64
             Ty3=-y3+32
             triangle = Triangle(int(Tx1), int(Ty1), int(Tx2), int(Ty2), int(Tx3), int(Ty3), outline=0xFFFF00)
-            splash.append(triangle)
+            splash.append(triangle)     #Prints the triange on the OLED
             return area
 
     except:
-        print("invalid input")
+        print("invalid input") #Runs if the try loop fails indicating there was an input error.
         avtext = f"Invalid input"
         text_area.text = avtext
         area = 0
@@ -84,11 +84,11 @@ def trianglearea (x1y1, x2y2, x3y3):
 
 while True:
     
-    coord1 = input('Coordinates for point 1:')
+    coord1 = input('Coordinates for point 1:') #Grabs user input and stores it
     coord2 = input('Coordinates for point 2:')
     coord3 = input('Coordinates for point 3:')
 
-    area = trianglearea(coord1, coord2, coord3)
+    area = trianglearea(coord1, coord2, coord3) #Calls the function.
     if area==0:
         continue
     
